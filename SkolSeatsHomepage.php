dc34f35cea79d7c5c7c3748d21552215859375e8
TheMack01 <Mackenzyjtaylor@gmail.com>
2023-03-27 12:59:57 -0500

    Update SkolSeatsHomepage.php

--- SkolSeatsHomepage.php
+++ SkolSeatsHomepage.php
@@ -4,7 +4,7 @@ session_start();
 
 //Header and links
 echo '<head>';
-echo '<center><a href="SkolSeatsHomepage1.php"><img src="SKOLSEATS1.JPG"><center></a><br>';
+echo '<center><a href="SkolSeatsHomepage.php"><img src="SKOLSEATS1.JPG"><center></a><br>';
 echo '<style>';
 echo 'fieldset {
     background-color: #7F00FF;
