024d5394d70d8d84e3cc695e174a8a4fbf68d06d
TheMack01 <Mackenzyjtaylor@gmail.com>
2023-03-27 12:15:32 -0500

    Update SkolSeatsHomepage.php

--- SkolSeatsHomepage.php
+++ SkolSeatsHomepage.php
@@ -4,7 +4,7 @@ session_start();
 
 //Header and links
 echo '<head>';
-echo '<center><a href="SkolSeatsHomepage.php"><img src="SKOLSEATS.JPG"><center></a><br>';
+echo '<center><a href="SkolSeatsHomepage1.php"><img src="SKOLSEATS1.JPG"><center></a><br>';
 echo '<style>';
 echo 'fieldset {
     background-color: #7F00FF;
@@ -63,7 +63,7 @@ echo '</head>';
 
 
 //Header and links
-echo '<body bgcolor="#FFFF00"><p style="display:inline-block; font-family:Arial Black; color:purple; position: fixed; left:8%;"><a href ="SkolSeatsHomepage.php">Home</a></p>';
+echo '<body bgcolor="#FFFF00"><p style="display:inline-block; font-family:Arial Black; color:purple; position: fixed; left:8%;"><a href ="SkolSeatsHomepage1.php">Home</a></p>';
 if(isset($_SESSION["userid"])) {
 	echo '<p align="left"; style="display:inline-block; font-family:Arial Black; color:purple; position: fixed; left:15%;"><a href ="SkolSignOut.php">Sign Out</a></p>';
 }
